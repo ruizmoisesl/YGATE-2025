2441c550d06e59b7b614e7ea9a83e45cb029b977
ruizmoisesl <moisesruiz2006m@gmail.com>
2024-12-10 15:48:58 -0500

    traduccion al español del mensaje del error

--- routes/errors.py
+++ routes/errors.py
@@ -1,4 +1,8 @@
 from flask import render_template, url_for, redirect, request
+from deep_translator import GoogleTranslator
+
+traductor = GoogleTranslator(source='auto', target='es')
 
 def error_not_found(e):
-    return render_template('404.html',error = e)
+    error_español= traductor.translate(str(e))
+    return render_template('404.html',error = error_español)
